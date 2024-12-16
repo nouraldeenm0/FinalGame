e5b344eebca8b4254716cc4280ee6895155e6d0b
Nour-aldeen <nouraldeenm0@gmail.com>
2024-12-16 19:25:12 +0200

    commit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,3 @@
-/*
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -12,87 +11,7 @@ public class Enemy : MonoBehaviour
     public int healthPoints;
     public int xpValue;
     public int damage;
-
-    // Aggression and Wanderer Reference
-    public Wanderer wanderer;    // Reference to the Wanderer script
-    public float campRange = 15f; // Range of the enemy camp
-    private NavMeshAgent agent;   // NavMeshAgent for movement
-
-    private void Start()
-    {
-        // Assign stats based on enemy type
-        if (enemyType == EnemyType.Minion)
-        {
-            healthPoints = 20;
-            xpValue = 10;
-            damage = 5;
-        }
-        else if (enemyType == EnemyType.Demon)
-        {
-            healthPoints = 40;
-            xpValue = 30;
-            damage = 10; // Default attack damage
-        }
-
-        agent = GetComponent<NavMeshAgent>();
-    }
-
-    private void Update()
-    {
-        if (wanderer == null) return;
-
-        float distanceToWanderer = Vector3.Distance(transform.position, wanderer.transform.position);
-
-        if (distanceToWanderer <= campRange)
-        {
-            // Aggressive behavior: move toward Wanderer
-            agent.SetDestination(wanderer.transform.position);
-
-            if (distanceToWanderer <= agent.stoppingDistance)
-            {
-                Attack();
-            }
-        }
-    }
-
-    private void Attack()
-    {
-        Debug.Log(enemyType + " attacks Wanderer!");
-        wanderer.TakeDamage(damage);
-    }
-
-    public void TakeDamage(int damageAmount)
-    {
-        healthPoints -= damageAmount;
-        Debug.Log(enemyType + " took damage! Health: " + healthPoints);
-
-        if (healthPoints <= 0)
-        {
-            Die();
-        }
-    }
-
-    private void Die()
-    {
-        Debug.Log(enemyType + " died! Wanderer gains " + xpValue + " XP.");
-        wanderer.GainXP(xpValue);
-        Destroy(gameObject);
-    }
-}
-*/
-using UnityEngine;
-using UnityEngine.AI;
-
-public class Enemy : MonoBehaviour
-{
-    // Enemy Types
-    public enum EnemyType { Minion, Demon }
-    public EnemyType enemyType;
-
-    // Stats
-    public int healthPoints;
-    public int xpValue;
-    public int damage;
+    public Animator anim;
 
     // Aggression and Wanderer Reference
     public Wanderer wanderer; // Reference to the Wanderer script
@@ -101,8 +20,11 @@ public class Enemy : MonoBehaviour
 
     public bool isAggressive = false; // Flag to control aggression
 
+
     private void Start()
     {
+        anim = GetComponent<Animator>();
+
         // Assign stats based on enemy type
         if (enemyType == EnemyType.Minion)
         {
@@ -161,6 +83,7 @@ public class Enemy : MonoBehaviour
     private void Attack()
     {
         Debug.Log(enemyType + " attacks Wanderer!");
+        anim.SetBool("punching", true);
         wanderer.TakeDamage(damage);
     }
 
